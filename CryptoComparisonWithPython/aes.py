96ed2a985bed530a5da8eaa7d4b381f22b4910c6
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2021-10-12 21:52:43 +0530

    made analysis of AES keyGen Times

---
+++ CryptoComparisonWithPython/aes.py
@@ -0,0 +1,27 @@
+import timeit
+
+if __name__ == "__main__":
+    fp = open("AES.csv","w")
+    headerKeyGen = '''
+from Crypto.Random import get_random_bytes
+    '''
+    code16 = '''
+key = get_random_bytes(16)
+    '''
+    code24 = '''
+key = get_random_bytes(24)
+    '''
+    code32 = '''
+key = get_random_bytes(24)
+    '''
+
+fp.writelines("keyGenAES(128),keyGenAES(192),keyGenAES(256)\n")
+keyGen128 = 0
+keyGen192 = 0
+keyGen256 = 0
+for i in range(100):
+    keyGen128 = timeit.timeit(setup=headerKeyGen, stmt=code16, number=10000)
+    keyGen192 = timeit.timeit(setup=headerKeyGen, stmt=code24, number=10000)
+    keyGen256 = timeit.timeit(setup=headerKeyGen, stmt=code32, number=10000)
+    fp.writelines(f"{keyGen128},{keyGen192},{keyGen256}\n")
+fp.close()
